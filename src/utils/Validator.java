fffc975c40344e6c8d4bfe636e78c1c53141a69d
Lenovo <fdhdsfhhsdkkhdsjj@kffkk.dkkf>
2021-11-26 18:09:45 +0300

    Fix mistakes
    Signed-off-by: Lenovo <fdhdsfhhsdkkhdsjj@kffkk.dkkf>

--- src/utils/Validator.java
+++ src/utils/Validator.java
@@ -2,53 +2,54 @@ package utils;
 
 import java.util.Scanner;
 
-public class Validator {
 
-        public static String validateName(Scanner scanner) {
-            String str = scanner.nextLine().trim();
-            while (str.isEmpty()) {
-                System.out.print("Пусто! Введите имя: ");
-                str = scanner.nextLine().trim();
-            }
-            return str;
+    public class Validator {
+    public static String validateName(Scanner scanner) {
+        String str = scanner.nextLine().trim();
+        while (str.isEmpty()) {
+        System.out.print("Пусто! Введите имя: ");
+        str = scanner.nextLine().trim();
+        }
+        return str;
         }
 
-        public static String validateQuantityInput(Scanner scanner) {
-            while (!scanner.hasNextInt()) {
-                String str = scanner.nextLine().trim();
-                System.out.printf("\"%s\" - не число!\n", str);
-                System.out.print("Введите количество!: ");
-            }
-            String quantity = scanner.nextInt();
-            while (quantity <= 0) {
-                System.out.println("Неверное значение! Введите количество: ");
-                while (!scanner.hasNextInt()) {
-                    String str = scanner.next().trim();
-                    System.out.printf("\"%s\" - не число!\n", str);
-                    System.out.println("Введите количество!: ");
-                }
-                quantity = scanner.nextInt();
-            }
-            return quantity;
+
+    public static int validateQuantityInput(Scanner scanner) {
+        while (!scanner.hasNextInt()) {
+        String str = scanner.nextLine().trim();
+        System.out.printf("\"%s\" - не число!\n", str);
+        System.out.print("Введите количество!: ");
+        }
+        int quantity = scanner.nextInt();
+        while (quantity <= 0) {
+        System.out.println("Неверное значение! Введите количество: ");
+        while (!scanner.hasNextInt()) {
+        String str = scanner.next().trim();
+        System.out.printf("\"%s\" - не число!\n", str);
+        System.out.println("Введите количество!: ");
+        }
+        quantity = scanner.nextInt();
+        }
+        return quantity;
         }
 
-        public static String validatesInput(Scanner scanner) {
-            while (!scanner.hasNextDouble()) {
-                String str = scanner.nextLine().trim();
-                System.out.printf("\"%s\" - не число!\n", str);
-                System.out.print("Введите цену!: ");
-            }
-            String price = scanner.nextDouble();
-            while (price <= 0) {
-                System.out.print("Неверное значение! Введите цену: ");
-                while (!scanner.hasNextDouble()) {
-                    String str = scanner.nextLine().trim();
-                    System.out.printf("\"%s\" - не число!\n", str);
-                    System.out.print("Введите цену!: ");
-                }
-                price = scanner.nextDouble();
-            }
-            return price;
-        }
-    }
 
+    public static int validatesInput(Scanner scanner) {
+        while (!scanner.hasNextInt()) {
+        String str = scanner.nextLine().trim();
+        System.out.printf("\"%s\" - не число!\n", str);
+        System.out.print("Введите цену!: ");
+        }
+        double price = scanner.nextInt();
+        while (price <= 0) {
+        System.out.print("Неверное значение! Введите цену: ");
+        while (!scanner.hasNextDouble()) {
+        String str = scanner.nextLine().trim();
+        System.out.printf("\"%s\" - не число!\n", str);
+        System.out.print("Введите цену!: ");
+        }
+        price = scanner.nextInt();
+        }
+        return (int) price;
+        }
+        }
