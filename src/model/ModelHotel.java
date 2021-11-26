da0f33d778b89bfd5d29d4fef681a0abbcbe9d65
Lenovo <fdhdsfhhsdkkhdsjj@kffkk.dkkf>
2021-11-26 13:49:04 +0300

    Create base model

---
+++ src/model/ModelHotel.java
@@ -0,0 +1,33 @@
+package model;
+
+import base.CountHotel;
+
+public class ModelHotel implements CountHotel {
+
+    private String name;
+    private int nights;
+    private int stars;
+    private int member;
+
+    public String getName() { return name; }
+
+    public void setName() { this.name = name; }
+
+    public int getNights() { return nights; }
+
+    public void setNights() { this.nights  = nights; }
+
+    public int getStars() { return stars; }
+
+    public void setStars() { this.stars = stars; }
+
+    public int getMember() { return member; }
+
+    public void setMember(int member) {this.member = member; }
+
+    @Override
+    public double countHotel(int star, int member, int night) {
+
+        return star + member + night ;
+    }
+}
