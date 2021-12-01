2eeac857fa6265d6bdd4a8551533bdd2093f999a
Dmitriy <dmitriyshulha@com>
2021-12-01 20:03:23 +0300

    Add 30% discount
    Signed-off-by: Dmitriy <dmitriyshulha@com>

--- src/count/HotelCount.java
+++ src/count/HotelCount.java
@@ -22,11 +22,15 @@ public class HotelCount {
 
      double member = model.countHotel(model.getAdult(),model.getChildren());
      double summ = model.countSumm(member, model.getStars(), model.getNights());
+     double discount = summ - (summ * 30 /100);
      String roundPayForHotel = Rounder.roundValue(summ);
+     String roundDIscount = Rounder.roundValue(discount);
 
      String output = "--------------------\n"+
              "Ваше имя - " + name + "\n" +
-             "Стоимость проживания за весь период - " + roundPayForHotel;
+             "Стоимость проживания за весь период - " + roundPayForHotel +
+             "\nCтоимость проживания с учетом скидки - " + roundDIscount;
+
 
      view.getOutput(output);
 
