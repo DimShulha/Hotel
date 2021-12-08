a5b7db2a1591246ed3fd2ea8d4f3e2ac38f025e5
Dmitriy <dmitriyshulha@com>
2021-12-08 13:13:28 +0300

    change reservation
    Signed-off-by: Dmitriy <dmitriyshulha@com>

--- src/reservation/Reserv.java
+++ src/reservation/Reserv.java
@@ -12,23 +12,23 @@ public class Reserv {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
 
         try {
             System.out.print("Номер комнаты: ");
             int roomNumber = sc.nextInt();
-            System.out.print("Дата заезда (dd/MM/yyyy): ");
+            System.out.print("Дата заезда (dd.MM.yyyy): ");
             Date checkIn = sdf.parse(sc.next());
-            System.out.print("Дата выезда (dd/MM/yyyy): ");
+            System.out.print("Дата выезда (dd.MM.yyyy): ");
             Date checkOut = sdf.parse(sc.next());
 
             ReservModel reservation = new ReservModel(roomNumber, checkIn, checkOut);
             System.out.println("Бронирование: " + reservation);
 
             System.out.println("\nВведите данные для обновления бронирования:");
-            System.out.print("Дата заезда (dd/MM/yyyy): ");
+            System.out.print("Дата заезда (dd.MM.yyyy): ");
             checkIn = sdf.parse(sc.next());
-            System.out.print("Дата выезда (dd/MM/yyyy): ");
+            System.out.print("Дата выезда (dd.MM.yyyy): ");
             checkOut = sdf.parse(sc.next());
 
             reservation.updateDates(checkIn, checkOut);
@@ -38,7 +38,7 @@ public class Reserv {
         } catch (ReservExeption e) {
             System.out.println("Ошибка при бронировании: " + e.getMessage());
         } catch (RuntimeException e) {
-            System.out.println("Неожиданная ошибка");
+            System.out.println("Oшибка");
         }
         sc.close();
     }
